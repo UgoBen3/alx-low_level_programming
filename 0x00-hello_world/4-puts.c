9b6d1f2dacc7b50e875ffb193fb33015d2ec5c7f
UgoBen3 <benedictwisdom3@gmail.com>
2022-06-30 17:27:27 -0700

    Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*Programming is like building a multilingual puzzle as said by ALX
+ *
+ * *return 0
+ *
+ * */
+
+int main (void){
+
+	puts ("\"programming is like building a multilingual puzzle");
+
+ 	return 0;
+
+}
