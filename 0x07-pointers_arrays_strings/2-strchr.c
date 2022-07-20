cbc1e4901592e1a24282eed0c4fcf926bdff4ee9
UgoBen3 <benedictwisdom3@gmail.com>
2022-07-19 18:23:54 -0700

    Task 2

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -11,15 +11,9 @@ char *_strchr(char *s, char c)
 	int i;
 
 	for (i = 0; *(s + i) != '\0'; i++)
-	{
 		if (*(s + i) == c)
-		{
-			return ((s + i));
-		}
-	}
-	if (*s == c)
-	{
-		return ((s + i));
-	}
-	return (0);
+			return (s + i);
+	if (*(s + i) == c)
+		return (s + i);
+	return ('\0');
 }
