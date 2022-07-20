c3776ff3089c8854d8c5b76a781d08973a1709ca
UgoBen3 <benedictwisdom3@gmail.com>
2022-07-19 22:55:53 -0700

    Task 6

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -6,16 +6,14 @@
  */
 void print_chessboard(char (*a)[8])
 {
-	unsigned int i, j = 0;
+	unsigned int i, j;
 
-	for (i = 0; i < 64; i++)
+	for (i = 0; i < 8; i++)
 	{
-		if (i % 8 == 0 && i != 0)
+		for (j = 0; j < 8; j++)
 		{
-			j = 1;
-			_putchar('\n');
+			_putchar(*(*(i + a) + j));
 		}
-		_putchar(a[i / 8][i - j]);
 	}
 	_putchar('\n');
 }
